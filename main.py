570a1566366030bdce27646c4158fd62cc442555
KyleRBoldman <32073292+KyleRBoldman@users.noreply.github.com>
2017-10-18 13:29:10 -0400

    woah

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from room import Room
 from flashlight import Flashlight
 from character import Enemy
 from container import Container
-<<<<<<< HEAD
 from weight_machine import Weight
-=======
 from gun import Gun
->>>>>>> fa47d7fbe8a0ebeb8c762918d5f55998367b6579
 
 heldItems = []
 myHealth = 53
@@ -54,11 +51,7 @@ supplycloset = Room("Supply Closet","A small dark room with a musty smell. On on
 #Armory Room
 #
 armory = Room("Armory Room","A dark open room that has a stinky stench. A glimmer catches your eye and you find a PISTOL. you find some other weapons but they are all broken.")
-<<<<<<< HEAD
-Pistol = Pistol(New, Pistol, 0)
-=======
 armory.create_room_item("gun")
->>>>>>> cec5ea543210084ec8aa551fafb3356db0ca005a
 
 #Ammo
 #
@@ -69,7 +62,7 @@ ammo.create_room_item("bullets")
 #
 fitnessroom = Room("Fitness Room","A small room with sets of weights and cardio equipment fit for a king! In the back, there is a large LOCKER, which appears to be unlocked. There is also a squat rack with a set of WEIGHTS you can LIFT")
 fitnessroom.locker = Container("locker",["fitness magazine", "5 pound dumbell"])
-squatrack = Weight()
+squatrack = Weight("Holds bar in a position used to initiate a squat, an exercise used to improve core strength.", 50)
 
 
 # Create a fake room called locked that represents all permenently locked doors
@@ -79,11 +72,7 @@ locked = Room("locked","")
 # Connect rooms. These are one-way connections.
 shop.link_room(locked, "EAST")
 shop.link_room(smalloffice, "SOUTH")
-<<<<<<< HEAD
 shop.link_room(fitnessroom, "WEST")
-=======
-shop.link_room(ammo, "WEST")
->>>>>>> fa47d7fbe8a0ebeb8c762918d5f55998367b6579
 shop.link_room(armory, "NORTH")
 supplycloset.link_room(smalloffice, "EAST")
 smalloffice.link_room(shop, "NORTH")
@@ -92,35 +81,11 @@ smalloffice.link_room(locked, "SOUTH")
 smalloffice.link_room(supplycloset, "WEST")
 lab.link_room(locked, "SOUTH")
 lab.link_room(smalloffice, "WEST")
-<<<<<<< HEAD
 armory.link_room(shop, "SOUTH")
 current_room = shop
 armory.link_room(shop, "SOUTH")
 fitnessroom.link_room(shop, "EAST")
-=======
 current_room = shop
-armory.link_room(shop, "SOUTH")
-current_room = shop
-lab.link_room(fitnessroom,"EAST")
-fitnessroom.link_room(lab,"WEST")
-current_room = kitchen
-armory.link_room(kitchen, "SOUTH")
-ammo.link_room(kitchen, "EAST")
-lab.link_room(fitnessroom,"EAST")
-fitnessroom.link_room(lab,"WEST")
-current_room = shop
-armory.link_room(shop, "SOUTH")
-ammo.link_room(shop, "EAST")
-lab.link_room(fitnessroom,"EAST")
-fitnessroom.link_room(lab,"WEST")
-current_room = shop
-armory.link_room(shop, "SOUTH")
-<<<<<<< HEAD
->>>>>>> fa47d7fbe8a0ebeb8c762918d5f55998367b6579
-
-
-=======
->>>>>>> cec5ea543210084ec8aa551fafb3356db0ca005a
 
 # Set up characters
 dmitry = Enemy("Dmitry", "A smelly zombie")
@@ -211,6 +176,9 @@ def checkUserInput(current_room,command,heldItems):
     elif current_room.name == "Laboratory" and command == "SHELF":
         # Open lab.shelf and concat each of the contents to the end of room_items
         current_room.room_items += lab.shelf.open()
+    elif current_room.name == "Fitness Room" and command == "LIFT WEIGHTS":
+        myHealth += squatRack.get_HP_per_rep
+        
 
     # ********************************* MOVE *********************************
     else:
--- weight_machine.py
+++ weight_machine.py
@@ -1,23 +1,26 @@
-class Weight():
-    def _init_(self, machine_name, machine_description):
-        self.name = machine_name
-        self.description = machine_description
+class Weight(machine_name,machine_description, weight):
+    def _init_(self, machine_name, machine_description, weight):
+        self.machine_name = machine_name
+        self.machine_description = machine_description
         self.weight = weight
+        HP_per_rep = 10
 
-    def set_description(self, machine_description):
-        self.description = machine_description
+    def get_weight(self,weight):
+        return self.weight
+        
+    def get_machine_name(self,machine_name):
+        return self.machine_name
 
-    def get_description(self):
-        return self.description
+    def get_HP_per_rep(self,HP_per_rep):
+        return self.HP_per_rep
 
-    def set_name(self, machine_name):
-        self.name = machine_name
+    def set_weight(self,newWeight):
+        self.weight = newWeight
 
-    def get_name(self):
-        return self.name
-    
-    def check_input(myHealth,command, current_room):
-        if command == "LIFT "+self.color.upper()+" WEIGHTS":
-            myHealth += 5
+    def set_HP_per_rep(self,newHP_per_rep):
+        self.HP_per_weight = newHP_per_rep
+
+        
+        
     
         
