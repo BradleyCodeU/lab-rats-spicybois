ab386bdbca6717dcdb0737d04304052d239a4f16
AbdullahIbrahim23 <AbdullahIbrahim23@users.noreply.github.com>
2017-10-23 13:28:53 -0400

    fixed gun

--- gun.py
+++ gun.py
@@ -3,15 +3,21 @@ class Gun():
         self.conditon = condition
         self.model = model
         self.ammo = 0
+        self.ammo = ammo
 
     def get_interface(self,heldItems,current_room):
+    def get_interface(self,ammo):
         if ammo == 0 and "BULLETS" in heldItems:
             print("You can LOAD BULLETS TO "+self.model())
         elif ammo == 0:
             print("You dont have ammo")
         if self.ammo > 0:
             print("You can REMOVE "+self.model()
+            print("You can REMOVE "+self.model)
 
     def check_input(self,command,heldItems,current_room):
+    def check_input(self):
         if command == "LOAD BULLETS TO "+self.model() and self.ammo == 0 and "bullets" in heldItems:
+            self.remove_bullets(heldItems)
+        if command == "UNLOAD BULLETS "+self.model() and self.ammo > 0:
             self.add_bullets(heldItems)
