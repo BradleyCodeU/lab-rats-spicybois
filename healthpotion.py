2987a3edda250ee55019231004125866b94762a5
TimmyS1 <timothy.solokha@hilliardschools.org>
2017-10-23 13:04:00 -0400

    Revert "Merge branch 'master' of https://github.com/BradleyCodeU/lab-rats-spicybois"
    This reverts commit 4e163c558cd78eb8dfb7364390d8c446469669fa, reversing
    changes made to 49a298a6d206144621ae3b3e17386f7187d3735b.

--- healthpotion.py
+++ healthpotion.py
@@ -2,16 +2,16 @@ from random import *
 
 class HealthPotion():
     #Constructs a name, the amount in the potion, and if it is large or small
-    def __init__(self, contain, category):
+    def __init__(self, name, contain, category):
+        self.name = name
         self.contain = randrange(0,6)
-        self.category = randrange(0,86)
-        
+        self.category = randrange(0,51)
     #Sets what kind of potion it is
     def get_potion_type(self):
         if self.category > 50:
-            print("Congrats! This heals for 50 health.")
+            print("Congrats! You found a large potion! This heals for 50 health.")
         elif self.category < 50:
-            print("This heals for 25 helath.")
+            print("A small potion heals for 25 helath.")
         else self.category == 0:
             print("Not your lucky day... there is no health potion...")
 
@@ -24,15 +24,7 @@ class HealthPotion():
         else:
             print("You have found many potions! You can take up to 7")
 
-    #Gets the name of the potion
-    def get_name(self,name):
-        if self.category > 50:
-            print("BIG POTION")
-        elif self.category < 50:
-            print("SMALL POTION)
-        else
-            print("NONE HAHAHA")
-                  
+    #Gets 
     
         
         
